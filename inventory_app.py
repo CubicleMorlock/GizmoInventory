b6921e342c98b3b9de798ca1b1cb3f923944df8c
noclues <noclues@apollo.com>
2025-01-15 02:42:55 +0000

    Minor cleanup

--- inventory_app.py
+++ inventory_app.py
@@ -1,23 +1,8 @@
-import re
-import configargparse
-
 from app import inventory_app
-from app.utils.logging import make_logger
-
-logger = make_logger()
+from app.config_load import config_load
 
 if __name__ == '__main__':
-    arg_parser = configargparse.ArgumentParser(default_config_files = ['./inventory_app.cfg'])
-    arg_parser.add_argument(
-        '--port',
-        dest = 'port',
-        env_var = 'INVENTORY_PORT',
-        default = 8080,
-        required = False,
-        help = 'inventory app port'
-    )
-
-    parsed_args, unknown_args = arg_parser.parse_known_args()
+    parsed_args = config_load()
 
     # used to do this using the netifaces module, picking the correct
     # interface to get the IP address, but that module requires a licensed
